830cd4d32d4961acf5817fb6d51322c3dd7c856f
RznAzn <sportrider84@gmail.com>
2017-07-21 13:21:54 -0600

    store ets date in shared Preferences

--- app/src/main/java/com/babykangaroo/android/freedomclock/EtsFragment.java
+++ app/src/main/java/com/babykangaroo/android/freedomclock/EtsFragment.java
@@ -6,7 +6,9 @@ import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,12 +26,15 @@ public class EtsFragment extends Fragment {
     private LocalDate etsDate;
     private ImageView setETSdate;
     private Context parentContext;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor spEditor;
 
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.ets_fragment, container, false);
         parentContext = getActivity();
+
         tvEtsDateView = (TextView) rootView.findViewById(R.id.tv_date_of_separation);
         tvDaysTillSeperation = (TextView) rootView.findViewById(R.id.tv_time_until);
         setETSdate = (ImageView) rootView.findViewById(R.id.iv_set_date);
@@ -47,15 +52,35 @@ public class EtsFragment extends Fragment {
                         int year = datePicker.getYear();
                         int month = datePicker.getMonth();
                         int day = datePicker.getDayOfMonth();
+                        spEditor = sharedPreferences.edit();
+                        spEditor.putInt(getString(R.string.year_pref), year);
+                        spEditor.putInt(getString(R.string.month_pref), month);
+                        spEditor.putInt(getString(R.string.day_pref), day);
                         etsDate = new LocalDate(year,month,day);
-                        tvEtsDateView.setText(etsDate.toString());
+                        spEditor.apply();
+                        setViews();
                     }
                 });
 
                 AlertDialog ad = builder.create();
                 ad.show();
             }
-        });
+        });sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        if (sharedPreferences.contains(getString(R.string.year_pref))
+                && sharedPreferences.contains(getString(R.string.month_pref))
+                && sharedPreferences.contains(getString(R.string.day_pref))){
+            setViews();
+
+        }
         return rootView;
+    }
+
+    private void setViews(){
+        etsDate = new LocalDate(sharedPreferences.getInt(getString(R.string.year_pref), 1970),
+            sharedPreferences.getInt(getString(R.string.month_pref), 1),
+            sharedPreferences.getInt(getString(R.string.day_pref), 1));
+        tvEtsDateView.setText(etsDate.toString());
+
+
     }
 }
