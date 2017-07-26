95ca89665df9a25335b58a5cc81c0cce69e13e53
RznAzn <sportrider84@gmail.com>
2017-07-25 18:00:07 -0600

    build out selfie function

--- app/src/main/java/com/babykangaroo/android/freedomclock/SelfieActivity.java
+++ app/src/main/java/com/babykangaroo/android/freedomclock/SelfieActivity.java
@@ -1,9 +1,12 @@
 package com.babykangaroo.android.freedomclock;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.provider.ContactsContract;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceManager;
@@ -20,14 +23,15 @@ import com.flurgle.camerakit.CameraView;
 public class SelfieActivity extends AppCompatActivity {
 
     private FrameLayout imageContainer;
-    private FrameLayout landingContainer;
+    private FrameLayout toDraw;
     private CameraView cameraView;
-    private TextView smile;
     private TextView timeLeft;
     private long daysLeft;
     private String message;
-    private ImageView takePicture;
+    private FloatingActionButton takePicture;
+    private FloatingActionButton retake;
     private ImageView selfieContainer;
+    private FloatingActionButton shareFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +47,14 @@ public class SelfieActivity extends AppCompatActivity {
         selfieContainer = (ImageView) findViewById(R.id.iv_selfie_container);
         timeLeft = (TextView) findViewById(R.id.tv_time_left);
         timeLeft.setText(message);
+
+        shareFab = (FloatingActionButton) findViewById(R.id.fab_share);
+        shareFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sharePhoto();
+            }
+        });
         cameraView = (CameraView) findViewById(R.id.cv_selfie);
         cameraView.setCameraListener(new CameraListener() {
             @Override
@@ -52,10 +64,10 @@ public class SelfieActivity extends AppCompatActivity {
             }
         });
         cameraView.setFacing(CameraKit.Constants.FACING_FRONT);
-        imageContainer = (FrameLayout) findViewById(R.id.fl_to_draw);
-        landingContainer = (FrameLayout) findViewById(R.id.fl_landing);
+        imageContainer = (FrameLayout) findViewById(R.id.fl_to_draw_container);
+        toDraw = (FrameLayout) findViewById(R.id.fl_to_draw_actual);
 
-        takePicture = (ImageView) findViewById(R.id.iv_photo);
+        takePicture = (FloatingActionButton) findViewById(R.id.fab_photo);
         takePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,13 +75,14 @@ public class SelfieActivity extends AppCompatActivity {
             }
         });
 
-        smile = (TextView) findViewById(R.id.tv_take_selfie);
-        smile.setOnClickListener(new View.OnClickListener() {
+        retake = (FloatingActionButton) findViewById(R.id.fab_retake);
+        retake.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showCamera();
             }
         });
+        showCamera();
     }
 
     @Override
@@ -84,15 +97,35 @@ public class SelfieActivity extends AppCompatActivity {
     }
 
     void showCamera(){
-        landingContainer.setVisibility(View.INVISIBLE);
         imageContainer.setVisibility(View.INVISIBLE);
         cameraView.setVisibility(View.VISIBLE);
         cameraView.start();
     }
 
     void showPhoto(){
-        landingContainer.setVisibility(View.INVISIBLE);
         imageContainer.setVisibility(View.VISIBLE);
         cameraView.setVisibility(View.INVISIBLE);
+    }
+
+    void sharePhoto(){
+
+        Bitmap bitmap = ConvertToBitmap(toDraw);String pathofBmp =
+                MediaStore.Images.Media.insertImage(this.getContentResolver(), bitmap,"title", null);
+        Uri bmpUri = Uri.parse(pathofBmp);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        intent.setType("image/png");
+        startActivity(intent);
+    }
+    protected Bitmap ConvertToBitmap(View layout) {
+        Bitmap map = Bitmap.createBitmap(layout.getMeasuredWidth(),
+                layout.getMeasuredHeight(),
+                Bitmap.Config.ARGB_8888);
+        layout.setDrawingCacheEnabled(true);
+        layout.buildDrawingCache();
+        Canvas canvas = new Canvas(map);
+        layout.draw(canvas);
+        return map;
     }
 }
